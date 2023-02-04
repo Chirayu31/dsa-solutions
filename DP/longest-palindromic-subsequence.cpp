04fc9879f4ed2d2b25d1df84899621e736e6957d
Chirayu <chirayupancholi17@gmail.com>
2023-02-04 21:02:04 +0530

    Solved 2 questions longest palindromic subsequence and print lcs

---
+++ DP/longest-palindromic-subsequence.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+private:
+    int solve(int ind1, int ind2, string &s, string &s2, vector<vector<int>> &dp)
+    {
+        if (ind1 < 0 || ind2 < 0)
+            return 0;
+
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+
+        if (s[ind1] == s2[ind2])
+            return dp[ind1][ind2] = 1 + solve(ind1 - 1, ind2 - 1, s, s2, dp);
+
+        return dp[ind1][ind2] = max(solve(ind1 - 1, ind2, s, s2, dp),
+                                    solve(ind1, ind2 - 1, s, s2, dp));
+    }
+
+public:
+    int longestPalindromeSubseq(string s)
+    {
+        int n = s.size();
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+        // vector<vector<int>> dp(n, vector<int>(n,-1));
+        // return solve(n-1, n-1, s, s2,dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (s[i - 1] == s2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][n];
+    }
+};
---
+++ DP/print-longest-common-subsequence.cpp
@@ -0,0 +1,77 @@
+class Solution
+{
+private:
+    int solve(int ind1, int ind2, string &text1, string &text2, vector<vector<int>> &dp)
+    {
+        if (ind1 < 0 || ind2 < 0)
+            return 0;
+
+        if (dp[ind1][ind2] != -1)
+            return dp[ind1][ind2];
+
+        if (text1[ind1] == text2[ind2])
+        {
+            return dp[ind1][ind2] = 1 + solve(ind1 - 1, ind2 - 1, text1, text2, dp);
+        }
+
+        return dp[ind1][ind2] = max(solve(ind1 - 1, ind2, text1, text2, dp), solve(ind1, ind2 - 1, text1, text2, dp));
+    }
+
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n1 = text1.size(), n2 = text2.size();
+        // vector<vector<int>> dp(n1, vector<int>(n2, -1));
+        // return solve(n1-1, n2-1, text1, text2, dp);
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+
+        for (int i = 0; i <= n1; i++)
+            dp[i][0] = 0;
+        for (int j = 0; j <= n2; j++)
+            dp[0][j] = 0;
+
+        for (int i = 1; i <= n1; i++)
+        {
+            for (int j = 1; j <= n2; j++)
+            {
+                if (text1[i - 1] == text2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int l = dp[n1][n2];
+        string ans = "";
+        for (int i = 0; i < l; i++)
+            ans += '_';
+
+        int ind = l - 1;
+        int i = n1, j = n2;
+
+        while (i > 0 && j > 0)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            {
+                ans[ind] = text1[i - 1];
+                ind--;
+                i--;
+                j--;
+            }
+            else if (dp[i - 1][j] > dp[i][j - 1])
+            {
+                i--;
+            }
+            else
+                j--;
+        }
+
+        cout << ans;
+
+        return dp[n1][n2];
+    }
+};
